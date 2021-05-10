8d185f8885846fb153ee866ee46a5ccc937c7815
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-05-10 20:25:58 -0300

    fix: rename styles in Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { SignInButton } from '../SignInButton'
 
-import styles from './stypes.module.scss'
+import './'
+import styles from './styles.module.scss'
 
 export function Header() {
   return (
