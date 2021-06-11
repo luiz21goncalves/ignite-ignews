c356098d5137e264052919eeec62da6ce2274529
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-06-11 20:29:28 -0300

    feat: validade active subscription

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -40,8 +40,17 @@ export default function Post({ post }: PostProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
-  const session = getSession({ req })
+  const session = await getSession({ req })
   const { slug } = params
+
+  if (!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
 
   const prismic = getPricmicClient(req)
 
